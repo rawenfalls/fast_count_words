2514456d40ee6cec2756ae462afa649e8cbbcf39
rawenfalls <zaqmond@yandex.ru>
2018-02-01 11:56:27 +0300

    v1.58

--- checker.rb
+++ checker.rb
@@ -44,11 +44,12 @@ end
 def ignored_word?(word)
   ignored_words = %w[на под за из по об над около при перед через от но хотя чтобы зато как да или либо]
   one_letter_words = %w[а б в ж и к о с у я]
-  if word.size == 1
-    !one_letter_words.any? { |letter| word.casecmp?(letter) }
-  elsif word == nil
-  else
-    ignored_words.any? { |ignored_word| word.casecmp?(ignored_word) }
+  case
+    when word.size == 1
+      !one_letter_words.any? { |letter| word.casecmp?(letter) }
+    when word == nil
+    else
+      ignored_words.any? { |ignored_word| word.casecmp?(ignored_word) }
   end
 end
 
